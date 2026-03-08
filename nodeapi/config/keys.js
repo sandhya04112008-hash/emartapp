26ab0e367f894f5c6db1b6eedf5caf69b9e26792
sandhya04112008-hash <sandhya04112008@gmail.com>
2026-03-08 13:15:54 +0530

    Add PersistentVolumeClaim for MongoDB and update deployment configurations

--- nodeapi/config/keys.js
+++ nodeapi/config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
- // mongoURI: "<INSERT DATABASE URI>",
-  //  mongoURI: "mongodb://emongo:27017/epoc",
-  mongoURI: "mongodb://emongo:27017/epoc",
+  mongoURI: process.env.MONGO_URI || "mongodb://emongo.emart.svc.cluster.local:27017/epoc",
   secretOrKey: "k1234e6s78h9av"
 };
